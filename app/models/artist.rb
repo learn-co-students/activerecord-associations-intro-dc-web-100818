cfd2988efed0d5cfbcc59f20550fab936c2f9faf
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-10-22 10:35:48 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,14 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
 end
+
+#has many songs; song belongs to an artist
+#has many genres through songs
+
+#songs is the join table
+
+#artist table will look like:
+#_________________________________
+#id             name
+#1             Taylor Swift
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,15 @@
 class Genre < ActiveRecord::Base
+  has_many :songs
+  has_many :artists, through: :songs
 end
+
+
+#has many songs
+#has many artists through songs
+
+#songs is a join table b/w artists and genres
+
+#genres table will look like
+#__________________________________
+#id               name
+#1                 pop
